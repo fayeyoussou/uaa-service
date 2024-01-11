d5f1ed28b14dbe89d2abce7f281a7c8fed218d27
fayeyoussou <fayeyoussoupha@gmail.com>
2024-01-11 09:58:12 +0000

    first commit

---
+++ src/main/java/sn/youdev/uaaservice/UaaServiceApplication.java
@@ -0,0 +1,13 @@
+package sn.youdev.uaaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaServiceApplication.class, args);
+	}
+
+}
